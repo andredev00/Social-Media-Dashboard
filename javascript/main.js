8eb95df5d60c9d68d66fbddbee571d02ab30f6d1
André Ferreira <andreferreira6578@gmail.com>
2021-02-14 21:50:59 +0000

    alterações de codigo para o front end e correção do overview

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,12 @@
 function toggleDemo() {
     halfmoon.toggleDarkMode();
-  }
+}
+
+function functionTwo () {
+    var url = "https://www.instagram.com/andrefr770";
+    request.get(url, function (err, response, body) {
+        if (response.body.indexOf(("meta property=\"og:description\" content=\"")) != -1) {
+            console.log("followers:", response.body.split("meta property=\"og:description\" content=\"")[1].split("Followers")[0])
+        }
+    });
+}
